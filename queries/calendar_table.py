6f937b7d2c18520f58432e3fda825862cf32b0b7
suaaa7 <spuaaa7@gmail.com>
2018-12-09 03:05:56 +0900

    add create_calendar_table.py

---
+++ queries/calendar_table.py
@@ -0,0 +1,48 @@
+DROP_CALENDAR_TABLE = '''
+DROP TABLE IF EXISTS calendars;
+'''
+
+CREATE_CALENDAR_TABLE = '''
+CREATE TABLE IF NOT EXISTS calendars (
+  calendar_id int(8),
+  year char(4),
+  url varchar(256),
+  title varchar(256),
+  parts int(8),
+  likes int(8),
+  subsc int(8),
+  items int(8),
+  actual_items int(8)
+);
+'''
+INSERT_CALENDAR_TABLE = '''
+INSERT INTO calendars (
+  calendar_id,
+  year,
+  url,
+  title,
+  parts,
+  likes,
+  subsc,
+  items,
+  actual_items
+)
+VALUES
+(
+  {calendar_id},
+  "{year}",
+  "{url}",
+  "{title}",
+  {parts},
+  {likes},
+  {subsc},
+  {items},
+  {actual_items}
+);
+'''
+
+SELECT_URL_TABLE = '''
+SELECT *
+FROM calendars;
+'''
+
